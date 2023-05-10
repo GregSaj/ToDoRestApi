60d8f91e62cccacc937fdf1ab7c158a9b591b4a3
koolzoli <grzegorz.sajewicz@gmail.com>
2023-05-10 16:49:59 +0200

    Add project files.

---
+++ ToDoRestApi/Controllers/ToDoModelsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToDoRestApi.Models;
+
+namespace ToDoRestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoModelsController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public ToDoModelsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ToDoModels
+        [HttpGet]
+        public async Task<ActionResult<List<ToDoModel>>> Index()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: ToDoModels/Details/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoModel>> Details(int? id)
+        {
+            if (id == null || _context.TodoItems == null)
+            {
+                return NotFound();
+            }
+
+            var toDoModel = await _context.TodoItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (toDoModel == null)
+            {
+                return NotFound();
+            }
+
+            return toDoModel;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<ToDoModel>> CreateToDo(ToDoModel addItem)
+        {
+            var toAdd = await _context.TodoItems.AddAsync(addItem);
+            await _context.SaveChangesAsync();
+            return toAdd.Entity;
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ToDoModel>> DeleteToDo(int? id)
+        {
+
+            var result = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (result == null)
+            {
+                return NotFound($"ToDo with Id = {id} not found");
+            }
+            else
+            {
+                _context.TodoItems.Remove(result);
+                await _context.SaveChangesAsync();
+                return result;
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ToDoModel>> UpdateToDo(ToDoModel toDo)
+        {
+
+            var toDoToUpdate = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == toDo.Id);
+
+            if (toDoToUpdate == null)
+            {
+                return NotFound($"ToDo with Id = {toDo.Id} not found");
+            }
+            else
+            {
+                //toDoToUpdate.Id = toDo.Id;
+                //toDoToUpdate.Name = toDo.Name;
+                //toDoToUpdate.Description = toDo.Description;
+                //toDoToUpdate.DueDate = toDo.DueDate;
+                //toDoToUpdate.Status = toDo.Status;
+
+                _context.TodoItems.Update(toDo);
+
+                await _context.SaveChangesAsync();
+
+                return toDoToUpdate;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ToDoModel>> UpdateStatusToDo(int id, bool isCompleted)
+        {
+
+            var toDoToUpdate = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (toDoToUpdate == null)
+            {
+                return NotFound($"ToDo with Id = {id} not found");
+            }
+            else
+            {
+                toDoToUpdate.Status = isCompleted;
+
+                await _context.SaveChangesAsync();
+
+                return toDoToUpdate;
+            }
+        }
+    }
+}
---
+++ ToDoRestApi/Models/ToDoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoRestApi.Models
+{
+    public class ToDoModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }        
+        public bool Status { get; set; }
+
+    }
+}
