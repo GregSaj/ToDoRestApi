b178a7ae4dfc616196f742a78edff52ff05329e5
koolzoli ^ <grzegorz.sajewicz@gmail.com>
2023-06-26 15:44:56 +0200

    Test changes

--- ToDoRestApi.Test/ToDoModelsControllerTests.cs
+++ ToDoRestApi.Test/ToDoModelsControllerTests.cs
@@ -1,37 +1,57 @@
+using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
 using Moq;
 using System.Net;
 using ToDoListDomain.Entities;
 using ToDoListDomain.RepositoryInterface;
 using ToDoRestApi.Controllers;
 
+
 namespace ToDoRestApi.Test
 {
     public class ToDoModelsControllerTests
     {
-        [Fact]
-        public async void GetAllItems_ReturnManyItems()
+        public async Task GetAllItems_ReturnManyItems()
         {
-            var toDoItems = new List<ToDoModel>()
-            { new ToDoModel { Id = 1, Name = "DoFirstThing"},
-            new ToDoModel { Id = 2, Name = "DoSecondThing"},
-            new ToDoModel { Id = 3, Name = "DoNextThing"}
-            };
-            var mockData = new Mock<IToDoRepository>();
-            mockData.Setup(e => e.GetAllItems()).ReturnsAsync(toDoItems);
+            // arrange
+            var repository = new Mock<IToDoRepository>();
+            repository.Setup(x => x.GetAllItems()).ReturnsAsync(GetTodoModelData);
+            var toDoController = new ToDoModelsController(repository.Object);
 
-            //arrange
+            // act
+            var getTodoResult = (OkObjectResult)await toDoController.GetAllItems();
 
-
-            //act
-            var result = new ToDoModelsController(mockData.Object);
-
-            //assert
-            Assert.Equal(2, await result.GetAllItems().Result);
+            /// Assert
+            getTodoResult.StatusCode.Should().Be(200);
 
         }
-        
+
+        private List<ToDoModel> GetTodoModelData()
+        {
+            List<ToDoModel> toDoModels = new List<ToDoModel>();
+            {
+                new ToDoModel
+                {
+                    Id = 1,
+                    Name = "DoFirstThing"
+                };
+                new ToDoModel
+                {
+                    Id = 1,
+                    Name = "DoNextThing"
+                };
+                new ToDoModel
+                {
+                    Id = 1,
+                    Name = "DoSecondThing"
+                };
+            }
+
+            return toDoModels;
+
+        }
     }
 }
